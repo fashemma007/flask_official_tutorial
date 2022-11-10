917e0359f9cf1adb03185677cd84d6f1c021b422
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-11-10 23:45:58 +0100

    auth: verifying user session status

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -89,3 +89,19 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+# registers a function that runs before the view function,
+# no matter what URL is requested.
+#==================================================================
+# load_logged_in_user() checks if a user id is stored in the session
+# and gets that user’s data from the database, storing it on g.user,
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
